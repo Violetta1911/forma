01a1d30a18a3d7512cf487b58dc3d25f43979136
Violetta1911 <sva198366@gmail.com>
2021-03-19 10:14:56 +0200

    started forma with Api

---
+++ main.js
@@ -0,0 +1,27 @@
+
+
+
+
+  const apiKey =
+  "ZxbEE5tR3REABDHNn4afJl3p9_xySoJTUWTv5oTZk7xgky1kQ63OvFlIVjw";
+
+//    const fetch = require('isomorphic-fetch');
+
+fetch("https://api.sheetson.com/v2/sheets/Notar", {
+  headers: {
+    Authorization: `Bearer ${apiKey}`,
+    "X-Spreadsheet-Id": "1dCYU20NVwq7HkpzUXdxvZTFdPbzemc1xOp3W_wv3hbQ",
+  },
+})
+  .then((r) => r.json())
+  .then((result) => {
+    console.log(result);
+    const city = result.results[23].City;
+    console.log(city);
+
+    let div = document.createElement("div");
+    div.className = "alert";
+    div.innerHTML = city;
+    document.body.append(div);
+  });
+   
